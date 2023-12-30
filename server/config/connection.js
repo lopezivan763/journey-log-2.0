761614fc4d123af7a36d2255f5a374a3fad692de
Ivan Lopez <84734876+lopezivan763@users.noreply.github.com>
2023-12-30 14:19:08 -0600

    updated server file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURI = process.env.MONGO_URI;
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/journey-log');
+ mongoURI,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 module.exports = mongoose.connection;
