05ec74450cb42fa08704ab9348d1c243eee9ba8c
Ivan Lopez <84734876+lopezivan763@users.noreply.github.com>
2023-12-30 14:51:07 -0600

    Pushing to Heroku

--- server/seeders/seed.js
+++
@@ -1,5 +0,0 @@
-// const db = require('../config/connection');
-// const { User, Post, Comment } = require('../models'); 
-// const userSeeds = require('./')
-// const postSeeds = require('./')
-// const 
