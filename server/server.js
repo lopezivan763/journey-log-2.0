a4da99b6e8c5ac3ab66133d2d7b54d401658f0d4
Ivan Lopez <84734876+lopezivan763@users.noreply.github.com>
2023-12-30 14:33:29 -0600

    make it better

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-require('dotenv').config();
-
-
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
